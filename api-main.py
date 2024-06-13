257a10ae49b6eb9cd70b724e725587d5e5b904e1
Sindhu Khrinsa <sindhu@sindo.ro>
2024-06-13 21:19:09 +0700

    add OrderForm functions

--- api-main.py
+++ api-main.py
@@ -27,9 +27,9 @@ from typing import Union
 from fastapi import FastAPI, Response, status
 from fastapi.staticfiles import StaticFiles
 from function.home import getHome
-from function.order import getCart
+from function.order import getCart, getOrderForm
 from function.auth import authLogin, authLogout, authRegister
-from function.classes import registrationForm, orderForm
+from function.classes import RegistrationForm, OrderForm
 from dotenv import load_dotenv
 import os
 load_dotenv()
@@ -45,40 +45,48 @@ def login(response: Response, username: str = "", password: str = ""):
     return authLogin(username=username, password=password)
 
 @app.get("/auth/logout", status_code=200)
-def logout(response: Response, username: str = "", apiKey: str = ""):
-    return authLogout(apiKey=apiKey)
+def logout(response: Response, apikey: str = ""):
+    return authLogout(apiKey=apikey)
 
 @app.post("/auth/register", status_code=200)
-def register(response: Response, registrationForm: registrationForm):
-    return authRegister(response=response, registrationForm=registrationForm)
+def register(response: Response, registrationform: RegistrationForm):
+    return authRegister(response=response, registrationForm=registrationform)
 
 #################################################################
 #                           HOME PAGE                           #
 #################################################################
 @app.get("/home", status_code=200)
-def home(response: Response, apiKey: str):
-    return getHome(response, apiKey)
+def home(response: Response, apikey: str):
+    return getHome(response, apikey)
 
 #################################################################
 #                            ORDERING                           #
 #################################################################
 
 @app.get("/cart", status_code=200)
-def cart(response: Response, apiKey: str = ""):
-    return getCart(response=response, apiKey=apiKey)
+def cart(response: Response, apikey: str = ""):
+    return getCart(response=response, apiKey=apikey)
 
 @app.get("/order", status_code=200)
-def order(response: Response, apiKey: str = ""):
-    return getOrder()
+def order(response: Response, apikey: str = "", producttype: str = ""):
+    return getOrderForm(response=response, apiKey=apikey, productType=producttype)
 
-@app.post("/order", status_code=200)
-def cart(response: Response, apiKey: str = "", orderForm: orderForm = {}):
-    return getCart(response=response, apiKey=apiKey, orderForm=orderForm)
+# @app.post("/order", status_code=200)
+# def cart(response: Response, apiKey: str = "", orderForm: orderForm = {}):
+#     return getCart(response=response, apiKey=apiKey, orderForm=orderForm)
 
-@app.get("/orderoverview", status_code=200)
+# @app.get("/orderoverview", status_code=200)
 
 
 #################################################################
 #                            ORDERING                           #
 #################################################################
-@app.get("/payment", status_code=200)
+# @app.get("/payment", status_code=200)
+
+
+@app.get("/test")
+def test(response: Response):
+    print (response.raw_headers)
+    return {
+        1:response.raw_headers
+    }
