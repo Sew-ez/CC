2153d109b73ac94979c7e741b376f08309ca06e2
Sewez-Team <dev@sewez.shop>
2024-06-13 13:03:30 +0000

    implement safety on query

--- function/database.py
+++ function/database.py
@@ -1,33 +1,53 @@
+##################################################################################################
+# MYSQL ORM
+#
+# Example usage for SELECT:
+# select_query = "SELECT * FROM users WHERE username = %s"
+# params = ("some_username",)
+# result, columns = runDB(select_query, params)
+#
+# Example usage for INSERT:
+# insert_query = "INSERT INTO users (username, password) VALUES (%s, %s)"
+# params = ("new_user", "hashed_password")
+# runDB(insert_query, params)
+#
+# Example usage for UPDATE:
+# update_query = "UPDATE users SET password = %s WHERE username = %s"
+# params = ("new_hashed_password", "existing_user")
+# runDB(update_query, params)
+#
+##################################################################################################
 import mysql.connector
-from dotenv import load_dotenv
-load_dotenv()
 import os
 
-def runDB(query: str):
+def runDB(query: str, parameters: tuple = ()):
     mydb = mysql.connector.connect(
-        host=os.getenv("DB_host"),
-        user=os.getenv("DB_user"),
-        password=os.getenv("DB_password"),
+        host=os.getenv("DB_Host"),
+        user=os.getenv("DB_User"),
+        password=os.getenv("DB_Password"),
         database=os.getenv("DB_Name")
     )
     mycursor = mydb.cursor()
 
+    myresult = []
+    column_names = []
+
     try:
-        mycursor.execute(query)
+        mycursor.execute(query, parameters)
+        
         if query.strip().upper().startswith(("UPDATE", "INSERT", "DELETE")):
             mydb.commit()
-        
-        myresult = mycursor.fetchall()
-        column_names = []
-        if mycursor.description is not None:
-            column_names = [column[0] for column in mycursor.description]
+        else:
+            myresult = mycursor.fetchall()
+            if mycursor.description is not None:
+                column_names = [column[0] for column in mycursor.description]
     except Exception as e:
         mydb.rollback()
         raise e
     finally:
         mycursor.close()
         mydb.close()
-        
+
     return (myresult, column_names)
 
 def DBtoDict(runDBResult, runDBColumn):
@@ -38,3 +58,30 @@ def DBtoDict(runDBResult, runDBColumn):
         row_dict = dict(zip(runDBColumn, row))
         data.append(row_dict)
     return data
+
+# def runDB(query: str):
+#     mydb = mysql.connector.connect(
+#         host=os.getenv("DB_Host"),
+#         user=os.getenv("DB_User"),
+#         password=os.getenv("DB_Password"),
+#         database=os.getenv("DB_Name")
+#     )
+#     mycursor = mydb.cursor()
+
+#     try:
+#         mycursor.execute(query)
+#         if query.strip().upper().startswith(("UPDATE", "INSERT", "DELETE")):
+#             mydb.commit()
+        
+#         myresult = mycursor.fetchall()
+#         column_names = []
+#         if mycursor.description is not None:
+#             column_names = [column[0] for column in mycursor.description]
+#     except Exception as e:
+#         mydb.rollback()
+#         raise e
+#     finally:
+#         mycursor.close()
+#         mydb.close()
+        
+#     return (myresult, column_names)
